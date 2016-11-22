d7b864766e06082fbb29b9ebd5fea12bc4edca92
lenn hypolite <lennhydesign@gmail.com>
2016-11-22 00:28:31 -0500

    DOne

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,5 +3,41 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
+    # # make a request to the API endpoint.
+    # @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+    #   # adding any parameters we need through the request object via a hash of params
+    #   req.params['client_id'] = "K0CYSKFTQFIIMKVECEHWQ1XZNNJZ5I55HR5D0U2RI1NCDJDU"
+    #   req.params['client_secret'] = "1YPCADSUDKNGFSAD0JG2WI3EZOUQ3OZHCBPRBIC0Z3KXWDH2"
+    #   req.params['v'] = '20160201'
+    #   req.params['near'] = params[:zipcode]
+    #   req.params['query'] = 'coffee shop'
+    # end
+    # body = JSON.parse(@resp.body)
+    # if @resp.success?
+    #   @venues = body["response"]["venues"]
+    # else
+    #   @error = body["meta"]["errorDetail"]
+    # end
+    begin
+     @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+         req.params['client_id'] =  "K0CYSKFTQFIIMKVECEHWQ1XZNNJZ5I55HR5D0U2RI1NCDJDU"
+         req.params['client_secret'] = "1YPCADSUDKNGFSAD0JG2WI3EZOUQ3OZHCBPRBIC0Z3KXWDH2"
+         req.params['v'] = '20160201'
+         req.params['near'] = params[:zipcode]
+         req.params['query'] = 'coffee shop'
+       end
+       body = JSON.parse(@resp.body)
+       if @resp.success?
+         @venues = body["response"]["venues"]
+       else
+         @error = body["meta"]["errorDetail"]
+       end
+
+     rescue Faraday::TimeoutError
+       @error = "There was a timeout. Please try again."
+     end
+    render 'search'
   end
+
 end
+# url = "https://api.foursquare.com/v2/venues/search?client_id=K0CYSKFTQFIIMKVECEHWQ1XZNNJZ5I55HR5D0U2RI1NCDJDU&client_secret=1YPCADSUDKNGFSAD0JG2WI3EZOUQ3OZHCBPRBIC0Z3KXWDH2&v=20161121&near=New York&query=coffee shops"
