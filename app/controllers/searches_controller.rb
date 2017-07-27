cd11ddb0964d1c14de491d41f7cc6a5b83387a9c
Mary <mumoh@mozilla.com>
2017-07-27 11:23:38 -0400

    Done.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,23 +3,25 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
-      @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+  begin
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
       req.params['client_id'] = '4QW25BRB00IJU3QO4RPJMZPYBZEZPC4UAQ23NJW4JCV4RERS'
       req.params['client_secret'] = 'YUT002BTSHLK3JJWL4OPGM5DLWTALLVE0K3KYBR1IBVJZGSD'
       req.params['v'] = '20160201'
       req.params['near'] = params[:zipcode]
       req.params['query'] = 'coffee shop'
-    end
+    end # end of get
 
-  body = JSON.parse(@resp.body)
-  if @resp.success?
-    @venues = body["response"]["venues"]
-  else
-    @error = body["meta"]["errorDetail"]
-  end
+    body = JSON.parse(@resp.body)
+    if @resp.success?
+        @venues = body["response"]["venues"]
+    else
+        @error = body["meta"]["errorDetail"]
+    end # end of if
 
-rescue Faraday::ConnectionFailed
-    @error = "There was a timeout. Please try again."
-  end
-  render 'search'
- end
+    rescue Faraday::ConnectionFailed
+        @error = "There was a timeout. Please try again."
+    end # end of rescue
+    render 'search'
+  end # end of method
+end # end of class
