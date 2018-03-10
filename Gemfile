1aab443dac138bc4ce5c815c03b22db0754d3b5d
smithwebtek <brad@smithwebtek.com>
2018-03-10 16:45:36 -0500

    Update Faraday lab

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 gem 'pry'
+gem 'dotenv-rails', require: 'dotenv/rails-now', groups: [:development, :test]
 gem 'faraday'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
@@ -23,6 +24,7 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -31,6 +33,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,30 +1,65 @@
 class SearchesController < ApplicationController
-  
+	
+
+	CLIENT_ID = 'BJ43KLAHMKUYVNKPJGYUOJSP1EBIKRIDUYTG10UPVTB1JCZC'
+	CLIENT_SECRET = 'BGQB4MZWRAUYF5VY0V5AZVLDCVP1SIBXJNU1IXX3BZNFPEON'
+
+
   def search
   end
 
-  def foursquare
-    client_id = '####################'
-    client_secret = '###################'
-  begin
-    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-        req.params['client_id'] = client_id
-        req.params['client_secret'] = client_secret
-        req.params['v'] = '20160201'
-        req.params['near'] = params[:zipcode]
-        req.params['query'] = 'coffee shop'
-        # req.options.timeout = 0
-      end
-      body = JSON.parse(@resp.body)
-      if @resp.success?
-        @venues = body["response"]["venues"]
-      else
-        @error = body["meta"]["errorDetail"]
-      end
- 
-    rescue Faraday::TimeoutError
-      @error = "There was a timeout. Please try again."
-    end
-    render 'search'
-  end
+	def foursquare
+		
+# raise params.inspect
+
+# https://api.foursquare.com/v2/venues/search?client_id=BJ43KLAHMKUYVNKPJGYUOJSP1EBIKRIDUYTG10UPVTB1JCZC&client_secret=BGQB4MZWRAUYF5VY0V5AZVLDCVP1SIBXJNU1IXX3BZNFPEON&v=20160201&near=03110&query=bagel&radius=900
+
+	  begin
+		@resp1 = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+			req.params['client_id'] = CLIENT_ID
+			req.params['client_secret'] = CLIENT_SECRET
+			req.params['v'] = '20160201'
+			req.params['near'] = params[:zipcode]
+			req.params['query'] = params[:query]
+			req.params['venuePhotos'] = 1
+			# req.params['radius'] = params["radius"].to_i
+			# req.options.timeout = 0
+		end
+		
+		body = JSON.parse(@resp1.body) 
+binding.pry
+		if @resp1.success?
+			@venues = body["response"]["venues"]
+		else
+			@error = body["meta"]["errorDetail"]
+		end
+		
+	rescue Faraday::TimeoutError
+		@error = "There was a timeout. Please try again."
+	end
+	render 'search'
+end
+
+
+def photos(id)
+	 url = https://api.foursquare.com/v2/venues/"#{id}"/photos
+	 begin
+		@resp2 = Faraday.get url do |req|
+			req.params['client_id'] = CLIENT_ID
+			req.params['client_secret'] = CLIENT_SECRET
+			req.params['v'] = '20160201'
+
+		end
+		body = JSON.parse(@resp2.body) 
+		if @resp2.success?
+			@venue_photos = [] # body["response"]["venues"]
+			binding.pry
+		else
+			@error = body["meta"]["errorDetail"]
+		end
+		rescue Faraday::TimeoutError
+			@error = "There was a timeout. Please try again."
+		end
+		render 'search'
+	end
 end
