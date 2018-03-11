024372d09ef9628c86349775fc523fcf3df61aad
smithwebtek <brad@smithwebtek.com>
2018-03-11 16:25:29 -0400

    Remove url from controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,8 +5,6 @@ class SearchesController < ApplicationController
 
 	def foursquare
 	
-# https://api.foursquare.com/v2/venues/search?client_id=BJ43KLAHMKUYVNKPJGYUOJSP1EBIKRIDUYTG10UPVTB1JCZC&client_secret=BGQB4MZWRAUYF5VY0V5AZVLDCVP1SIBXJNU1IXX3BZNFPEON&v=20160201&near=03110&query=bagel&radius=900
-
 	  begin
 		@resp1 = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
 			req.params['client_id'] = ENV["FS_CLIENT_ID"]
@@ -15,8 +13,8 @@ class SearchesController < ApplicationController
 			req.params['near'] = params[:zipcode]
 			req.params['query'] = params[:query]
 			# req.params['venuePhotos'] = 1
-			# req.params['radius'] = params["radius"].to_i
-			# req.options.timeout = 100000
+			# req.params['radius'] = '500'
+			# req.options.timeout = 10
 		end
 		
 		body = JSON.parse(@resp1.body) 
@@ -34,12 +32,34 @@ class SearchesController < ApplicationController
 end
 
 
-def photos(id)
+def venue_detail(id)
+	 url = https://api.foursquare.com/v2/venues/"#{id}"
+	 begin
+		@resp2 = Faraday.get url do |req|
+			req.params['client_id'] = ENV["FS_CLIENT_ID"]
+			req.params['client_secret'] = ENV["FS_CLIENT_SECRET"]
+			req.params['v'] = '20160201'
+
+		end
+		body = JSON.parse(@resp2.body) 
+		if @resp2.success?
+			@venue_photos = [] # body["response"]["venues"]
+			binding.pry
+		else
+			@error = body["meta"]["errorDetail"]
+		end
+		rescue Faraday::TimeoutError
+			@error = "There was a timeout. Please try again."
+		end
+		render 'search'
+	end
+
+	def photos(id)
 	 url = https://api.foursquare.com/v2/venues/"#{id}"/photos
 	 begin
 		@resp2 = Faraday.get url do |req|
-			req.params['client_id'] = CLIENT_ID
-			req.params['client_secret'] = CLIENT_SECRET
+			req.params['client_id'] = ENV["FS_CLIENT_ID"]
+			req.params['client_secret'] = ENV["FS_CLIENT_SECRET"]
 			req.params['v'] = '20160201'
 
 		end
