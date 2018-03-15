bfe09b2876ab9a2c5175c0cead4abe75071216d2
BuffaloRob <peckham.rob@gmail.com>
2018-03-15 08:37:06 -0400

    removed id/secret

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,8 +5,8 @@ class SearchesController < ApplicationController
   def foursquare
     begin
       @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-        req.params['client_id'] = '2UDQI0YTKCLGLNI3JHR0UA2IFKK0TOCATZ0GVKHZNF3JIBR0'
-        req.params['client_secret'] = 'T5UASBO0L1MW4FOEW0C5AWLFHLVKLR2BAMS1LABMZQBPZFRX'
+        req.params['client_id'] = ''
+        req.params['client_secret'] = ''
         req.params['v'] = '20160201'
         req.params['near'] = params[:zipcode]
         req.params['query'] = 'coffee shop'
