7ccae641471b604cd54cc40b00a7a6203d2cd137
Joshua Needham <joshuabneedham@gmail.com>
2018-04-09 19:35:17 -0600

    Done.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -18,6 +18,8 @@ class SearchesController < ApplicationController
     else
       @error = body["meta"]["errorDetail"]
     end
+  rescue Faraday::ConnectionFailed
+    @error = "There was a timeout. Please try again."
     render 'search'
   end
 end
