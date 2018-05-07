53548388e768ce5fa055d735265c81d2ab2656c6
NolanHughes <Nhughes987@gmail.com>
2018-05-06 19:04:20 -0500

    Delete client id and secret.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,8 +5,8 @@ class SearchesController < ApplicationController
   def foursquare
     begin
       @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-        req.params['client_id'] = 'IK3WDNLLE22ZXOVMQO5FXFUZJ003HMWOCENS2QOIT5EWXX33'
-        req.params['client_secret'] = 'TGWX5GWD1TVKKHDDCS23TD1ACFRH34UOF34OODDO0KTW0W5T'
+        req.params['client_id'] = 'ID'
+        req.params['client_secret'] = 'SECRET'
         req.params['v'] = '20160201'
         req.params['near'] = params[:zipcode]
         req.params['query'] = 'coffee shop'
