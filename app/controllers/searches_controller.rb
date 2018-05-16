0a655cfe8a047df3d484c77b766284a653c5edde
bretburau <github email address>
2018-05-16 06:29:21 -0500

    Done.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,5 +3,39 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
+  #   @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+  #     req.params['client_id'] = 'IYKA2IC2AHC2ZQSP11BFFGDFGCWHV5RG5ZJGBCIPQZQMWU4T'
+  #     req.params['client_secret'] = 'YXZAQHQE4Y0ZCCYXM1TUAWKDW1Q5HLYWLA1XPC4ZGTDSNAVO'
+  #     req.params['v'] = '20160201'
+  #     req.params['near'] = params[:zipcode]
+  #     req.params['query'] = 'coffee shop'
+  #   end
+  #   body = JSON.parse(@resp.body)
+  #   if @resp.success?
+  #     @venues = body["response"]["venues"]
+  #   else
+  #     @error = body["meta"]["errorDetail"]
+  #   end
+  #   render 'search'
+  # end
+  begin
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+        req.params['client_id'] = 'IYKA2IC2AHC2ZQSP11BFFGDFGCWHV5RG5ZJGBCIPQZQMWU4T'
+        req.params['client_secret'] = 'YXZAQHQE4Y0ZCCYXM1TUAWKDW1Q5HLYWLA1XPC4ZGTDSNAVO'
+        req.params['v'] = '20160201'
+        req.params['near'] = params[:zipcode]
+        req.params['query'] = 'coffee shop'
+      end
+      body = JSON.parse(@resp.body)
+      if @resp.success?
+        @venues = body["response"]["venues"]
+      else
+        @error = body["meta"]["errorDetail"]
+      end
+ 
+    rescue Faraday::ConnectionFailed
+      @error = "There was a timeout. Please try again."
+    end
+    render 'search'
   end
 end
