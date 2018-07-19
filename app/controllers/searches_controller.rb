6ae539c6e538e0c68ad7cb3c993ba18d985ae789
Emily Jirles <emily.jirles@gmail.com>
2018-07-19 19:15:41 +0000

    Done.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,8 +5,8 @@ class SearchesController < ApplicationController
   def foursquare
     begin
       @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-        req.params['client_id'] = '4KJBAQ5FMO3Z0EJP4OWNYFTB4JBMOTP40Z3J1SZJZ2E2WCPS'
-        req.params['client_secret'] = '3ZBEF2CZLQDPZDADQ31T2CJSXEUHOCMTXYRU3KPO0D1GFRWM'
+        req.params['client_id'] = 'client_id' #=> removed actual client_id
+        req.params['client_secret'] = 'client_secret' #=> removed actual client secret
         req.params['v'] = '20160201'
         req.params['near'] = params[:zipcode]
         req.params['query'] = 'coffee shop'
