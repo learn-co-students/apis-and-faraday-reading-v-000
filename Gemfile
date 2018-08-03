7b19614c1d3c2cb6cfc3884204afcccb68850155
Connor Gallopo <connor.gallopo@me.com>
2018-08-03 10:02:01 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'coffee-rails', '~> 4.1.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+
+gem 'faraday'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
@@ -44,4 +46,3 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,5 +3,15 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
-  end
+      @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+        req.params['client_id'] = "LVCHKQDD4NFURZ0LKNNAWEPEXFX103DHJRDQK13P5WKVSS53"
+        req.params['client_secret'] = "M45A3KC5ZODWUMSBBPO5MTIEELN32RG535QL0DJLTJ3VPAXI"
+        req.params['v'] = '20160201'
+        req.params['near'] = params[:zipcode]
+        req.params['query'] = 'coffee shop'
+      end
+      body_hash = JSON.parse(@resp.body)
+      @venues = body_hash["response"]["venues"]
+      render 'search'
+    end
 end
