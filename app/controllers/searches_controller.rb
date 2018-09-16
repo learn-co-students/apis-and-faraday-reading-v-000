39d3e1166d47f17377573221d2be9c921ff365b8
barbara.prusiewicz prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-16 22:32:45 +0000

    Done.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,18 @@
 class SearchesController < ApplicationController
+  @@client_id = 'BGYBPONDSHZNVM42WWU343RTR1T4TZ2XNQJT52CLEYJL13N5'
+  @@client_secret = '1TIHATF0J0FBVHCBS0PG2QXOCH2N4SH01AVQ30L1PO5YKD4E'
+
   def search
   end
 
   def foursquare
+    Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+      req.params['client_id'] = @@client_id
+      req.params['client_secret'] = @@client_secret
+      req.params['v'] = '20160201'
+      req.params['near'] = params[:zipcode]
+      req.params['query'] = 'coffee shop'
+    end
+    render 'search'
   end
 end
