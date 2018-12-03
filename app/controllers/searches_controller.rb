e6bb26105289b63208bddf817d76a83fa2ee1b8d
roitblatari <roitblatari@gmail.com>
2018-12-03 12:32:07 -0500

    Done.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,17 +3,42 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
+    # @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+    #   req.params['client_id'] = 'IQ0HZ4JSZ1BSTZGXJYACXZOD35BEJGTV5CMBAEUQOJ2YY5QK'
+    #   req.params['client_secret'] = '2QF1VPFW5V2BPHK0ZOZH2BGJMKDFTKQH3F0KB3CPEKOUP3O5'
+    #   req.params['v'] = '20160201'
+    #   req.params['near'] =  params[:zipcode]
+    #   req.params['query'] = 'coffee shop'
+    #
+    # end
+    #   body = JSON.parse(@resp.body)
+    # if @resp.success?
+    #   @venues = body["response"]["venues"]
+    # else
+    #   @error = body["meta"]["errorDetail"]
+    # end
+    # render 'search'
+    begin
     @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-      req.params['client_id'] = 'IQ0HZ4JSZ1BSTZGXJYACXZOD35BEJGTV5CMBAEUQOJ2YY5QK'
-      req.params['client_secret'] = '2QF1VPFW5V2BPHK0ZOZH2BGJMKDFTKQH3F0KB3CPEKOUP3O5'
-      req.params['v'] = '20160201'
-      req.params['near'] #= params[:zipcode]
-      req.params['query'] = 'coffee shop'
-    end
+        req.params['client_id'] = 'IQ0HZ4JSZ1BSTZGXJYACXZOD35BEJGTV5CMBAEUQOJ2YY5QK'
+        req.params['client_secret'] = '2QF1VPFW5V2BPHK0ZOZH2BGJMKDFTKQH3F0KB3CPEKOUP3O5'
+        req.params['v'] = '20160201'
+        req.params['near'] = params[:zipcode]
+        req.params['query'] = 'coffee shop'
+        # req.options.timeout = 0
+      end
+      body = JSON.parse(@resp.body)
+      if @resp.success?
+        @venues = body["response"]["venues"]
+      else
+        @error = body["meta"]["errorDetail"]
+      end
 
-    body_hash = JSON.parse(@resp.body)
-    @venues = body_hash["response"]["venues"]
+    rescue Faraday::ConnectionFailed
+      @error = "There was a timeout. Please try again."
+    end
     render 'search'
+
   end
 
 end
