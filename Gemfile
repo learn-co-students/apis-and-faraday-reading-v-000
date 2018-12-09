76b7b102f4e13ecd26cefc7b6b94b30e53f74065
Xplor8r <mpieschel39@gmail.com>
2018-12-08 19:56:47 -0800

    Done

--- Gemfile
+++ Gemfile
@@ -46,3 +46,4 @@ group :development do
 end
 
 gem 'faraday'
+gem 'dotenv-rails'
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,12 +3,23 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
-    Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-      req.params['client_id'] = JRGAB4RKU3OY3EMUUDK4244QMOH4T0ZKQC3G2SIRSTB40LYV
-      req.params['client_secret'] = 52G1B4KKPC4PNWQ1FOGDIQHCSXMAVQQXLBAPOHUIDWHCEJK3
-      req.params['v'] = '20160201'
-      req.params['near'] = params[:zipcode]
-      req.params['query'] = 'coffee shop'
+    begin
+      @response = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+        req.params['client_id'] = 'JRGAB4RKU3OY3EMUUDK4244QMOH4T0ZKQC3G2SIRSTB40LYV'
+        req.params['client_secret'] = '52G1B4KKPC4PNWQ1FOGDIQHCSXMAVQQXLBAPOHUIDWHCEJK3'
+        req.params['v'] = '20160201'
+        req.params['near'] = params[:zipcode]
+        req.params['query'] = 'coffee shop'
+      end
+      body = JSON.parse(@response.body)
+      if @response.success?
+        @venues = body["response"]["venues"]
+      else
+        @error = body["meta"]["errorDetail"]
+      end
+ 
+    rescue Faraday::ConnectionFailed
+      @error = "There was a timeout. Please try again."
     end
     render 'search'
   end
