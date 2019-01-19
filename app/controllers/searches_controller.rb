adfab3c637434e8c90df83e7beec2471b91045dc
github username <github email address>
2019-01-19 12:20:14 -0600

    Done.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,22 +5,23 @@ class SearchesController < ApplicationController
   def foursquare
     begin
     @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-        req.params['client_id'] = client_id
-        req.params['client_secret'] = client_secret
-        req.params['v'] = '20160201'
-        req.params['near'] = params[:zipcode]
-        req.params['query'] = 'coffee shop'
-        req.options.timeout = 0
-      end
-      body = JSON.parse(@resp.body)
-      if @resp.success?
-        @venues = body["response"]["venues"]
-      else
-        @error = body["meta"]["errorDetail"]
-      end
+      req.params['client_id'] = 'CJ533OYTNV0XHAQO0PNQWGAYNI1NN5CT05JSVE1L4PZO2ES2'
+      req.params['client_secret'] = 'EGSB2V5MQJB5MKYC5R0ADHM35OA125GPQ2DKCJQ1LDIRQRMU'
+      req.params['v'] = '20160201'
+      req.params['near'] = params[:zipcode]
+      req.params['query'] = 'coffee shop'
+      # req.options.timeout = 0
+    end
+   body = JSON.parse(@resp.body)
+   if @resp.success?
+     @venues = body["response"]["venues"]
+   else
+     @error = body["meta"]["errorDetail"]
+   end
 
-    rescue Faraday::ConnectionFailed
+   rescue Faraday::ConnectionFailed
       @error = "There was a timeout. Please try again."
     end
-    render 'search'
+   render 'search'
+  end
 end
