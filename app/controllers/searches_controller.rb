0c8d05342b399989550dc8187c43c26873662e43
Bruce Hillsworth <brucehillsworth@Bruces-MacBook-Air.local>
2019-03-10 16:04:27 -0600

    Readme Completed

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,5 +3,24 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
+    begin
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+        req.params['client_id'] = 'VOGGTSVICCUP5SSZ5CPFTE22TWX4TWRR2ZLRVUNWMW1UFEEV'
+        req.params['client_secret'] = 'JRH30FN5CPXJQUXWHWRNNPIKQTJ0N1CP21HGCD20GBT3BDJQ'
+        req.params['v'] = '20160201'
+        req.params['near'] = params[:zipcode]
+        req.params['query'] = 'coffee shop'
+      end
+      body = JSON.parse(@resp.body)
+      if @resp.success?
+        @venues = body["response"]["venues"]
+      else
+        @error = body["meta"]["errorDetail"]
+      end
+
+    rescue Faraday::ConnectionFailed
+      @error = "There was a timeout. Please try again."
+    end
+    render 'search'
   end
 end
