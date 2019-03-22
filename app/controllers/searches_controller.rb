e23b6826d84b7bb4ccfd698c6df2782393886407
Marc Mills <mdmills1990@gmail.com>
2019-03-22 11:19:08 -0400

    removed API client keys

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,8 +5,8 @@ class SearchesController < ApplicationController
   def foursquare
     begin
       @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-        req.params['client_id'] = 'GIBJOEDWMA1GD4OE1QGX3EU1C3CGFKEJWDGFNS0GX2FYCXYI'
-        req.params['client_secret'] = 'OQZD5A2FOXO1H5WUCT3BMNBEWJB3LOBSVMIDG5ON0GED0F2C'
+        req.params['client_id'] = '#'
+        req.params['client_secret'] = '##'
         req.params['v'] = '20160201'
         req.params['near'] = params[:zipcode]
         req.params['query'] = 'coffee shop'
