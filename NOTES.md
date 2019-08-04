7bcc576b02db7b0bf55fed382c27aaba595e6270
github username <github email address>
2019-08-04 10:44:01 -0700

    Done.

---
+++ NOTES.md
@@ -0,0 +1,5 @@
+// Through Postman a GET API request using the parameter near and query
+
+https://api.foursquare.com/v2/venues/search?client_id=2OCBHRPNYEVVRUKR4VLHX11OJ2Y0FQKZX1UQHYIIF4BJMYFP
+
+&client_secret=H0K5TPV1YLNZGC33PSLHRSZ1FMVO1M5QAB4GGXITDI0UYZE1&v=20190804&near=fairfax,ca&query=coffee
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,28 @@ class SearchesController < ApplicationController
   def search
   end
 
+
   def foursquare
-  end
+    begin
+      @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+        req.params['client_id'] = 'CLIENT_ID'
+        req.params['client_secret'] = 'CLIENT_SECRET'
+        req.params['v'] = '20190804'
+        req.params['near'] = params[:zipcode]
+        req.params['query'] = 'coffee shop'
+        # req.options.timeout = 0
+
+      end
+      body = JSON.parse(@resp.body)
+      if @resp.success?
+        @venues = body["response"]["venues"]
+      else
+        @error = body["meta"]["errorDetail"]
+      end
+
+      rescue Faraday::ConnectionFailed
+      @error = "There was a timeout. Please try again."
+    end
+      render 'search'
+    end
 end
